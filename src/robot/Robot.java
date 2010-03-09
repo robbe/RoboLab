59fab0e6aa0dfa819f038acdc201950fbfa8a400
Martin 'felbit' Knopf <knopf.martin@gmail.com>
2010-03-09 16:55:40 +0100

    Some Pseudocode in RobotControl

--- src/robot/Robot.java
+++ src/robot/Robot.java
@@ -1,19 +1,12 @@
 package robot;
 
 import lejos.nxt.*;
+import lejos.robotics.navigation.*;
 
 public class Robot {
 
-	public void move() {
-		Motor.A.forward();
-		Motor.B.forward();
-	}
-	
-	public boolean isOnLine() {
-		boolean onLine = true;
-		LightSensor light = new LightSensor(SensorPort.S1);
-		
-		return onLine;
-	}
+	static final float WHEEL_DIAM = 5.6f; 
+	static final float TRACK_W = 13f;
+	static Pilot pil = new Pilot(WHEEL_DIAM, TRACK_W, Motor.A, Motor.B);
 	
 }
--- src/robot/RobotControl.java
+++ src/robot/RobotControl.java
@@ -4,4 +4,23 @@ import lejos.nxt.*;
 
 public class RobotControl {
 
+	final static Robot trash_E = new Robot();
+	
+	/* So sollte es ablaufen:
+	 * 
+	 * 
+	 * 
+	 * Main {
+	 *   Wenn trash_E.getLichtSensorWert() < Grenzwert {
+	 *     trash_E ist online
+	 *   }
+	 *   Wenn trash_E.getLichtSensorWert() > Grenzwert {
+	 *     trash_E ist offline
+	 *     finde die Linie
+	 *   }
+	 *   Folge der Linie
+	 *   
+	 * }
+	 */
+	
 }
