2ae02fbb2ed23bc75d73a755ac59549d3d8d1698
Martin 'felbit' Knopf <knopf.martin@gmail.com>
2010-03-09 20:16:42 +0100

    More Pseudocode in Robot.java

--- src/robot/Robot.java
+++ src/robot/Robot.java
@@ -5,8 +5,44 @@ import lejos.robotics.navigation.*;
 
 public class Robot {
 
-	static final float WHEEL_DIAM = 5.6f; 
-	static final float TRACK_W = 13f;
-	static Pilot pil = new Pilot(WHEEL_DIAM, TRACK_W, Motor.A, Motor.B);
+	static final float WHEEL_DIAM = 5.5f; 
+	static final float TRACK_W = 12f;
+	static Pilot pil = new Pilot(WHEEL_DIAM,TRACK_W,Motor.A,Motor.B);
+	
+	public void travel() {
+		pil.travel(40);
+		
+		/* TODO:
+		 * 
+		 * ActionListener für Sensorik einbauen
+		 * 
+		 */
+		
+	}
+	
+	public boolean isOnLine() {
+		boolean online = true;
+		
+		/* TODO:
+		 * 
+		 * Logik zur Abfrage ob sich der Roboter
+		 * noch auf der Linie befindet.
+		 * 
+		 * Wenn er die Linie verlässt:
+		 * online = false;
+		 * 
+		 */
+		
+		return online;
+	}
+	
+	/* TODO:
+	 * 
+	 * ActionListener
+	 * -- offlineAction
+	 * -- touchAction
+	 * -- ultrasonicAction
+	 * 
+	 */
 	
 }
