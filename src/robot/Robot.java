41a6999d86e8ff910ca97958859c2c15a4e8535e
Martin 'felbit' Knopf <knopf.martin@gmail.com>
2010-03-09 21:23:53 +0100

    Search line & rotate methods

--- src/robot/Robot.java
+++ src/robot/Robot.java
@@ -4,6 +4,7 @@ import lejos.nxt.*;
 
 public class Robot {
 
+	/* Eventuell muessen hier noch die Sensoren angepasst werden. */
 	private TouchSensor touchl = new TouchSensor(SensorPort.S1);
 	private TouchSensor touchr = new TouchSensor(SensorPort.S2);
 	private LightSensor light = new LightSensor(SensorPort.S3);
@@ -18,6 +19,47 @@ public class Robot {
 		Motor.B.forward();
 		
 		Thread.sleep(100);
+		
+		Motor.A.stop();
+		Motor.B.stop();
+		
+	}
+	
+	/* TODO:
+	 * 
+	 * Die Rotationen lassen sich sicher einfacher und eleganter lšsen, indem man eine Methode 
+	 * "rotate(int degree) {...}"
+	 * erstellt und die Richtung durch positive || negative Werte bestimmt.
+	 * 
+	 */
+	public void rotateLeft(int deg) throws InterruptedException {
+		
+		int time = deg * 10;	// Der Wert muss noch angepasst werden, eventuell durch Ausprobieren
+								// Masochisten kšnnen sich ja mal am Ausrechnen versuchen. Wert sollte durch Radgrš§e, Radabstand und Motorrotationsgeschwindigkeit ermittelbar sein.
+		Motor.A.setPower(100);
+		Motor.B.setPower(100);
+		
+		Motor.A.backward();
+		Motor.B.forward();
+		
+		Thread.sleep(time);
+		
+		Motor.A.stop();
+		Motor.B.stop();
+		
+	}
+	
+	public void rotateRight(int deg) throws InterruptedException {
+		
+		int time = deg * 10;	// Der Wert muss noch angepasst werden, eventuell durch Ausprobieren
+								// Masochisten kšnnen sich ja mal am Ausrechnen versuchen. Wert sollte durch Radgrš§e, Radabstand und Motorrotationsgeschwindigkeit ermittelbar sein.
+		Motor.A.setPower(100);
+		Motor.B.setPower(100);
+		
+		Motor.B.backward();
+		Motor.A.forward();
+		
+		Thread.sleep(time);
 		
 		Motor.A.stop();
 		Motor.B.stop();
--- src/robot/RobotControl.java
+++ src/robot/RobotControl.java
@@ -4,11 +4,36 @@ public class RobotControl {
 
 	public static void main() throws InterruptedException {
 		
-		Robot trash_E = new Robot();
+		Robot rob = new Robot();
 		
-		while(trash_E.isOnLine() && !trash_E.hasTouchedRight() && !trash_E.hasTouchedLeft()) {
-			trash_E.travel();
+		/* Startet der Roboter auf der Linie? */
+		if(!rob.isOnLine()) {
+			/* Nein.
+			 * Wir müssen die Linie suchen!
+			 * 
+			 * Ablauf:
+			 *   Geradeausfahrten von 1/10 sec.
+			 *   Nachsehen, ob die Linie erreicht wurde
+			 *   ^ 10 mal wiederholen
+			 *   
+			 *   Drehen (welcher Winkel?!)
+			 *   Alles wiederholen
+			 * 
+			 */
+			while(!rob.isOnLine()) {
+				for(int i = 0;i < 10; i++) {
+					while(!rob.isOnLine())
+						rob.travel();
+				}
+			
+				rob.rotateLeft(90);
+			}
+			
 		}
+		
+		/* Auf der Linie fahren. */
+		while(rob.isOnLine() && !rob.hasTouchedRight() && !rob.hasTouchedLeft())
+			rob.travel();
 	
 	}
 	
