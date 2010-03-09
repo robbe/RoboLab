2434e0ad6c96d71a24ec995721baea0e2d528886
Martin 'felbit' Knopf <knopf.martin@gmail.com>
2010-03-09 23:08:30 +0100

    Ultrasonic Class, error fixed: Press both touchsensors leads to unspecified behavior

--- src/robot/RobotControl.java
+++ src/robot/RobotControl.java
@@ -5,31 +5,38 @@ public class RobotControl {
 	public static void main() throws InterruptedException {
 		
 		Robot rob = new Robot();
+		Sonic son = new Sonic();
 		
 		while(true) {	// Achtung: Endlosschleife!
 			
 			/* Auf der Linie fahren. */
-			while(rob.isOnLine() && !rob.hasTouchedRight() && !rob.hasTouchedLeft())
+			while(rob.isOnLine() && !rob.hasTouched())
 				rob.travel();
 			
 			/* Robot stoppt - was soll getan werden? */
+			/* Stopp, weil nicht auf der Linie */
 			if(!rob.isOnLine()) {
 				
 				// Tue etwas, damit der Roboter auf die Linie zurückfindet
+				// Hier muss ein bisschen KungFoo rein, damit wir die gestrichelte
+				// Linie schaffen ...
 				
-			} else if(rob.hasTouchedLeft) {
+			/* Stopp, weil Hindernis */
+			} else if(rob.hasTouched) {
 				
-				rob.rotateRight(90);
+				rob.moveBack(200);			// ca. 4cm rueckwaerts bewegen
+				rob.rotateRight(90);		// 90° Drehung des Roboters nach rechts
 				
-				// Hier kommt der Ultraschallsensor ins Spiel
-				// Hindernis muss umfahren werden 
-				
-			} else if(rob.hasTouchedRight) {
-				
-				rob.rotateLeft(90);
-				
-				// Hier kommt der Ultraschallsensor ins Spiel
-				// Hindernis muss umfahren werden 
+				son.rotate(-90);			// 90° Drehung des USonic-Sensors nach links
+				while(!rob.isOnLine()) {	// Sonlange er die Linie nicht gefunden hat
+					
+					while(son.echo()) {		// ... aber ein Echo erhält
+						rob.travel();		// faehrt er geradeaus
+					}
+											// Kein Echo?
+					rob.rotateLeft(5);		// 5° Drehung des Roboters nach links
+											// Suche nach neuem Echo oder Linie
+				}
 				
 			}
 			
